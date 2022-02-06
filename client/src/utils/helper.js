ccd122bd52481d9348d75a94ca4bc3a2d2db25ba
Mike Rogers <mrogers23458@gmail.com>
2022-02-06 13:23:28 -0800

    added auth for decoding webtoken, and helper.js with REGEX for validating email and password

---
+++ client/src/utils/helper.js
@@ -0,0 +1,10 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+export function validatePass(password) {
+    const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    return re.test(String(password))
+  }
+  
