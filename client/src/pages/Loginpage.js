f86f81bac79257d53467b1af5653beabdc33aff6
Mike Rogers <mrogers23458@gmail.com>
2022-02-06 13:20:52 -0800

    added state variables to track inputs for sending to db, added a view password icon and function

--- client/src/pages/Loginpage.js
+++ client/src/pages/Loginpage.js
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import Grassfooter from '../components/Grassfooter'
 
 export default function Loginpage() {
 
-    const logFun = function (e) {
-        e.preventDefault()
-        console.log('working')
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleInputUpdate = function (e) {
+        console.log(e.target.name)
+        setCredentials(
+            {
+                ...credentials, [e.target.name]: e.target.value
+            }
+        )
+
+        console.log(credentials)
     }
+
+    const togglePasswordVisibility = function () {
+        const password = document.querySelector('.login-password-inp')
+        const eyeIcon = document.querySelector('.bi-eye-slash')
+
+        console.log(eyeIcon)
+
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type)
+
+        eyeIcon.classList.toggle('bi-eye')
+    }
+
     return (
         <div className="home-box">
             <div className="home-header-box">
@@ -14,11 +39,14 @@ export default function Loginpage() {
             </div>
             <div className="login-form-box">
                 <form className="login-form">
-                    <label className="login-username label">Username</label>
-                    <input type="text" className="login-username input" placeholder="Username"></input>
+                    <label className="login-username-lab label">Username</label>
+                    <input type="text" className="login-username-inp input" onChange={handleInputUpdate} placeholder="Username" name="username"></input>
                     <label type="text" className="login-password label">Password</label>
-                    <input type="text" className="login-password input" placeholder="password"></input>
-                    <button onClick={logFun} className="login-btn btn" type="submit">Login</button>
+                    <div className='pass-box'>
+                    <input type="password" className="login-password-inp input" onChange={handleInputUpdate} placeholder="password" name="password"></input>
+                    <i className="bi bi-eye-slash" id="togglePassword" onClick={togglePasswordVisibility}></i>
+                    </div>
+                    <button className="login-btn btn" type="submit">Login</button>
                 </form>
             </div>
             <Grassfooter />
